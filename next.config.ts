0cdccfb5501217f03507031ecfb2a9d0c2dc6378
ioilmio <illuminato.salvatore@gmail.com>
2026-06-09 10:24:44 +0200

    fix: conditional basePath for dev vs prod

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/nailstech',
+  basePath: process.env.NODE_ENV === 'production' ? '/nailstech' : '',
   images: {
     unoptimized: true,
   },
